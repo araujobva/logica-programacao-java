5f7a50485328b4f71f13ca8e79eccc0f3d1328a0
Bruno <araujobva@gmail.com>
2018-11-27 00:18:25 -0200

    Resolvendo problema de soma de 4 valores

---
+++ EstruturaLinear/Soma4Valores.java
@@ -0,0 +1,15 @@
+//Problema: Faça um programa que receba quatro números inteiros, calcule e mostre a soma desses números.
+
+import javax.swing.JOptionPane;
+
+class Soma4Valores{
+    public static void main(String[] args){
+        int valor1, valor2, valor3, valor4, soma;
+        valor1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor inteiro"));
+        valor2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo valor inteiro"));
+        valor3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro valor inteiro"));
+        valor4 = Integer.parseInt(JOptionPane.showInputDialog("Digite o quarto valor inteiro"));
+        soma = valor1 + valor2 + valor3 + valor4;
+        JOptionPane.showMessageDialog(null,"A soma dos quatro valores e de "+soma);
+    }
+}
