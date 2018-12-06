019b83619f58668b1ad416644e98917bbdf4f214
Bruno <araujobva@gmail.com>
2018-12-06 06:22:17 -0200

    Resolvendo problema sobre média aritmética

--- EstruturaLinear/MediaAritmetica.java
+++ EstruturaLinear/MediaAritmetica.java
@@ -1,12 +1,14 @@
 //Problema: Faça um programa que receba três notas, calcule e mostre a média aritmética entre elas.
 
+import javax.swing.JOptionPane;
+
 class MediaAritmetica{
-    public static void main calculaMedia(){
+    public static void main (String[] args){
         float nota1, nota2, nota3, media;
-        nota1 = JOptionPane.showInputDialog("Digite a primeira nota");
-        nota2 = JOptionPane.showInputDialog("Digite a segunda nota");
-        nota3 = JOptionPane.showInputDialog("Digite a terceira nota");
+        nota1 = Float.parseFloat(JOptionPane.showInputDialog("Digite a primeira nota"));
+        nota2 = Float.parseFloat(JOptionPane.showInputDialog("Digite a segunda nota"));
+        nota3 = Float.parseFloat(JOptionPane.showInputDialog("Digite a terceira nota"));
         media = (nota1 + nota2 + nota3) / 3;
-        JOptionPane.showMessageDialog("A média aritmética é de "+media);
+        JOptionPane.showMessageDialog(null,"A media aritmetica e de "+media);
     }
 }
