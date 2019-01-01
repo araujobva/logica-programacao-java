8274347fc500b24c7d3db135a5ac738d7262d8e6
Bruno <araujobva@gmail.com>
2019-01-01 01:13:33 -0200

    Resolvendo problema salario a receber.

---
+++ EstruturaLinear/SalarioReceber.java
@@ -0,0 +1,4 @@
+/*
+ * Problema: Faça um programa que receba o salário-base de um funcionário, calcule e mostre o salário a receber, sabendo-se que esse funcionário 
+ * tem gratificação de 5% sobre o salário-base e paga imposto de 7% sobre o salário-base. 
+ */
