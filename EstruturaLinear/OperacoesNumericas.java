1180f7fdc90f1920316116b86953da0bdc314114
Bruno Araújo <araujobva@gmail.com>
2019-01-02 10:35:51 -0200

    Resolvendo várias operações com números.

--- EstruturaLinear/OperacoesNumericas.java
+++ EstruturaLinear/OperacoesNumericas.java
@@ -1,4 +1,5 @@
-/* Problema: Faça um programa que receba um número positivo e maior que zero, calcule e mostre:
+/* 
+ * Problema: Faça um programa que receba um número positivo e maior que zero, calcule e mostre:
  * a) O número digitado ao quadrado.
  * b) O número digitado ao cubo.
  * c) A raiz quadrada do número digitado.
