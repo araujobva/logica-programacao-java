bee9f8cd89ce4400fdc52a1105dd821c6fdfaf87
Bruno <araujobva@gmail.com>
2019-01-01 23:26:26 -0200

    Mostrando a mensagem Olá Mundo no console.

--- EstruturaLinear/OlaMundo.java
+++ EstruturaLinear/OlaMundo.java
@@ -1,7 +1,7 @@
-//Problema: Mostre a mensagem "Olá Mundo" no console.
+//Problema: Mostre a mensagem "Olá Mundo!" no console.
 
 class OlaMundo{
     public static void main(String[] args){
-        System.out.println("Ola Mundo");
+        System.out.println("Ola Mundo!");
     }
 }
