9b4d4428330274b4844a4c0f769086891b206dc4
Bruno Araújo <araujobva@gmail.com>
2019-02-15 12:24:47 -0200

    Mostrando 200 valores com for.

---
+++ EstruturaRepeticao/ValoresParesFor.java
@@ -0,0 +1,8 @@
+//Problema: Mostre todos os valores pares positivos até 200 com a estrutura de repetição For.
+
+class ValoresParesFor{
+    public static void main(String args[]){
+        for(int i = 2; i <= 200; i = i + 2)
+            System.out.println(i);
+    }
+}
