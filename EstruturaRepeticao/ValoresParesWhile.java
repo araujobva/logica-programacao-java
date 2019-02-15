21f709e89d611cf4442298bbbacd9e814c410407
Bruno Araújo <araujobva@gmail.com>
2019-02-15 12:25:03 -0200

    Mostrando 200 valores com While.

---
+++ EstruturaRepeticao/ValoresParesWhile.java
@@ -0,0 +1,11 @@
+//Problema: Mostre todos os valores pares positivos até 200 com a estrutura de repetição While.
+
+class ValoresParesWhile{
+    public static void main(String[] vetor){
+        int i = 2;
+        while(i <= 200){
+            System.out.println(i);
+            i = i + 2;
+        }
+    }
+}
