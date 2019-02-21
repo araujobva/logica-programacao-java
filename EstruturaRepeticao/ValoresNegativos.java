a6a9b384d1e376d3770deea5b4af432593371e31
Bruno Araújo <araujobva@gmail.com>
2019-02-21 14:38:18 -0300

    Mostrando valores negativos entre -1 e -100.

---
+++ EstruturaRepeticao/ValoresNegativos.java
@@ -0,0 +1,9 @@
+//Problema: Faça um programa que que mostre os valores negativos entre 1 e 100.
+
+class ValoresNegativos{
+    public static void main(String[] args){
+        for(int i = -1; i >= -100; i--){
+            System.out.println(i);
+        }
+    }
+}
