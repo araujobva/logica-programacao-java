24345131a0ef3e4404abb50460d226a3d487b6e9
Bruno <araujobva@gmail.com>
2019-08-30 05:11:52 -0300

    Mostrando 5 valores múltiplos de 3.

---
+++ EstruturaRepeticao/Valores3em3.java
@@ -0,0 +1,11 @@
+//Começando do 0, mostre 5 valores múltiplos de 3.
+
+import javax.swing.JOptionPane;
+
+class Valores3em3{
+	public static void main(String[] args){
+		for(int i = 0; i < 15; i = i + 3){
+			System.out.print(i+" - ");
+		}
+	}
+}
