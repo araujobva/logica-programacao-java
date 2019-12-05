6b87cb83d68c39b6bc76de70bab79dc1955abdb0
Bruno Araújo <araujobva@gmail.com>
2019-12-05 14:13:45 -0300

    Mostrando a mensagem Olá Mundo de maneira gráfica.

---
+++ EstruturaLinear/OlaMundoGrafico.java
@@ -0,0 +1,11 @@
+//Problema: Mostre a mensagem "Olá Mundo" de maneira gráfica.
+
+import javax.swing.JOptionPane;
+
+class OlaMundoGrafico{
+	
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null,"Olá Mundo");
+	}
+
+}
