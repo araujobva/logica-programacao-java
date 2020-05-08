641ba77214855a49fde15bc5efac59d1a0ab84cc
Bruno Araújo <araujobva@gmail.com>
2020-05-08 08:02:39 -0300

    Catálogo simples de livros.

---
+++ TodasEstruturas/CatalogoLivros/Main.java
@@ -0,0 +1,7 @@
+//Problema: Faça um programa para catalogar livros, o programa deverá conter o nome do livro e o autor.
+
+class Main{
+  public static void main(String[] args){
+    Interface.interface();
+  }
+}
