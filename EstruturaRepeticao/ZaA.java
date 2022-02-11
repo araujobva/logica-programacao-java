f057ce038c444cc14ef940b331d1bea0e309c71b
Bruno Araújo <araujobva@gmail.com>
2022-02-10 21:56:51 -0300

    Imprimindo o alfabeto ao contrário.

---
+++ EstruturaRepeticao/ZaA.java
@@ -0,0 +1,11 @@
+/*
+ * Problema: Imprima o alfabeto ao contrário, ou seja, de Z a A.
+ */
+
+class ZaA {
+	public static void main(String[] args){
+		for(char i = 'z'; i >= 'a'; i--){
+			System.out.println(i);
+		}
+	}
+}
