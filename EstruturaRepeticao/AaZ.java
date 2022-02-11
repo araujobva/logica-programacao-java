33c4416be2c19dc0b60c4e28f02a1e33324864c1
Bruno Araújo <araujobva@gmail.com>
2022-02-10 21:56:27 -0300

    Imprimindo o alfabeto.

---
+++ EstruturaRepeticao/AaZ.java
@@ -0,0 +1,14 @@
+/*
+ * Problema: Faça um programa que imprima o alfabeto.
+ */
+
+class AaZ{
+	public static void main(String[] args){
+		for(char i = 'a'; i <= 'z'; i++){
+			System.out.println(i);
+		}
+	}
+}
+
+
+
