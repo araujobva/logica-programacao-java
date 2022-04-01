91675a19c3b8c99c519e16723db8c9f6da7d3239
Bruno Araújo <araujobva@gmail.com>
2022-03-31 23:18:05 -0300

    Emulando um software para venda de casas.

--- TodasEstruturas/VendaDeCasas/Menu.java
+++ TodasEstruturas/VendaDeCasas/Menu.java
@@ -1,5 +1,10 @@
 class Menu{
     public static void menu(){
-        
+        System.out.println("=== Menu ===");
+        System.out.println("Digite 1 - Para cadastrar um imóvel.");
+        System.out.println("Digite 2 - Para remover um imóvel de um respectivo dono.");
+        System.out.println("Digite 3 - Para remover um dono com todos os seus imóveis.");
+        System.out.println("Digite 4 - Para pesquisar todos os impoveis de um respectivo dono.");
+        System.out.println(">> ");
     }
 }
