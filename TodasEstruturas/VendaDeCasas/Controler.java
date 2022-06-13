80c552d682630f59404935f9b1de7cfe4aba4687
Bruno Araújo <araujobva@gmail.com>
2022-06-13 19:43:24 -0300

    Emulando um software para venda de casas

---
+++ TodasEstruturas/VendaDeCasas/Controler.java
@@ -0,0 +1,17 @@
+class Controler{
+    public static void cadastraImovel(){
+
+    }
+
+    public static void removeImovel(){
+
+    }
+
+    public static void removeDonoTodosImoveis(){
+
+    }
+
+    public static void pesquisaImovel(){
+        
+    }
+}
--- TodasEstruturas/VendaDeCasas/Menu.java
+++ TodasEstruturas/VendaDeCasas/Menu.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 class Menu{
     public static void menu(){
         System.out.println("=== Menu ===");
         System.out.println("Digite 1 - Para cadastrar um imóvel.");
         System.out.println("Digite 2 - Para remover um imóvel de um respectivo dono.");
         System.out.println("Digite 3 - Para remover um dono com todos os seus imóveis.");
-        System.out.println("Digite 4 - Para pesquisar todos os impoveis de um respectivo dono.");
-        System.out.println(">> ");
+        System.out.println("Digite 4 - Para pesquisar todos os imóveis de um respectivo dono.");
+        System.out.print(">> ");
+        Scanner read = new Scanner(System.in);
+        int op;
+        op = read.nextInt();
+        switch(op){
+            case 1:
+                Controler.cadastraImovel();
+                break;
+            case 2:
+                Controler.removeImovel();
+                break;
+            case 3:
+                Controler.removeDonoTodosImoveis();
+                break;
+            case 4:
+                Controler.pesquisaImovel();
+                break;
+            default:
+                System.out.print("Esta opção não é válida");
+        }
     }
 }
