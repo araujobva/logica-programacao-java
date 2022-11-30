32bd0d99983921e8c952cd30c02fba5c41addb5a
Bruno Araújo <araujobva@gmail.com>
2022-11-30 13:25:49 -0300

    Cadastro dupla de tenista.

--- TodasEstruturas/CadastroTenista/Main.java
+++ TodasEstruturas/CadastroTenista/Main.java
@@ -1,8 +1,8 @@
 /*
  * Problema: Crie um algoritmo para gerenciar o nome de uma dupla de tenistas, o software deverá:
  * 1 - Adicionar o nome e o cpf dos 2 tenistas.
- * 2 - Ter a opção de excluir um determinado tenista.
- * 3 - Listar todos os tenistas.
+ * 2 - Listar todos os tenistas.
+ * 3 - Ter a opção de excluir um determinado tenista.
  * 4 - Se tiver uma vaga, permiter que adiciona um novo tenista.
  */
 
