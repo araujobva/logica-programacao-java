c94cb03dd8e4aba5327df5e2f180cea5d11d581b
Bruno Araújo <araujobva@gmail.com>
2023-01-03 12:54:23 -0300

    Imprimindo valor de A a -100.

---
+++ EstruturaRepeticao/Valor100AMenos100.java
@@ -0,0 +1,11 @@
+/*
+ * Problema: Faça um algoritmo que imprima o valor de 100 a -100.
+ */
+
+class Valor100AMenos100 {
+	public static void main(String[] args) {
+		for(int i = 100; i >= -100 ; i--) {
+			System.out.println(i);
+		}	
+	}
+}
